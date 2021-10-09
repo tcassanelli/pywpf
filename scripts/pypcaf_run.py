207ef002f86a9e3bbea0e2b489a6b38ba66a3904
Tomas Cassanelli <tcassanelli@protonmail.com>
2021-10-09 17:19:20 -0400

    preparing run scinet

--- scripts/pypcaf_run.py
+++ scripts/pypcaf_run.py
@@ -9,8 +9,8 @@ import pypcaf
 files = [
     'n0', 'n25', 'n50', 'n75', 'n100', 'n200', 'n250', 'n275', 'n300', 'n325'
     ]
-series = ['000', '001']
-merit_func = [pypcaf.merit2, pypcaf.merit3]
+series = ['000', '001', '002']
+merit_func = [pypcaf.merit1, pypcaf.merit2, pypcaf.merit3]
 
 # pypcaf.pcaf_double(
 #     time_path='../../data_pulsar2/{}.npy'.format(name),
@@ -25,24 +25,25 @@ merit_func = [pypcaf.merit2, pypcaf.merit3]
 #     work_dir='../../data_pulsar2/'
 #     )
 
-
 for f in files:
 
-    # for m in merit_func:
-    #     pypcaf.pcaf_single(
-    #         time_path='../../data_pulsar2/{}.npy'.format(f),
-    #         dt=0.002793,
-    #         T_init=0.089367,
-    #         iteration=1000,         # 10 us scanning
-    #         delta=1e-8,             # 10 ns step
-    #         num_div=range(3, 21),
-    #         merit_func=m,
-    #         region_order=3,
-    #         work_dir='../../data_pulsar2/'
-    #         )
+    for m in merit_func:
+        pypcaf.pcaf_single(
+            time_path=f'/scratch/v/vanderli/cassane/data_pulsar/{f}.npy',
+            dt=0.002793,
+            T_init=0.089367,
+            iteration=1000,         # 10 us scanning
+            delta=1e-8,             # 10 ns step
+            num_div=range(3, 21),
+            merit_func=m,
+            region_order=3,
+            work_dir='/scratch/v/vanderli/cassane/data_pulsar/'
+            )
 
     for s in series:
         pypcaf.plot_all(
-            pypcaf_path='../../data_pulsar2/pypcaf_out/{}-{}'.format(f, s),
+            pypcaf_path=(
+                f'/scratch/v/vanderli/cassane/data_pulsar/pypcaf_out/{f}-{f}'
+                ),
             T_ref=0.08936715
             )
---
+++ scripts/run_slurm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=12:00:00
+#SBATCH --job-name PCAF
+#SBATCH --output=/scratch/v/vanderli/cassane/output/mpi_ex_%j.txt
+#SBATCH --mail-type=ALL
+
+module load texlive
+
+export MPLCONFIGDIR="$MPLCONFIGDIR:$SCRATCH" 
+
+cd $SCRATCH
+
+# INPUT 
+file="pypcaf_run.py"
+
+directory="/home/v/vanderli/cassane/PCA-Folding/scripts/"
+path2file=$directory$file
+
+# python environment
+APY3="/home/v/vanderli/cassane/anaconda3/bin/python"
+
+$APY3 $file
