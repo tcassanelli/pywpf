33ad00c65333fa023da2a72162348464e3c9154d
Tomas Cassanelli <tcassanelli@protonmail.com>
2021-11-13 21:07:05 -0500

    renaming the package

--- pypcaf/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Author: Tomas Cassanelli
-from .pcaf import *
-from .plot_routines import *
-from .pcaf_functions import *
-from .merit_functions import *
-from .epochf import *
